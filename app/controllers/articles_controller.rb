87cc2d2d75a25108f26cd3ee711a4204f8c7c2e9
Ahmed Ammar <ammaramphersoft@gmail.com>
2023-07-31 17:05:09 +0500

    Crud Opreations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,57 @@
 class ArticlesController < ApplicationController
-def show
-    binding.break
-    @article = Article.find(params[:id])
-end
+    
+    before_action :set_article, only: [:show,:edit,:update,:destroy]
+    
+    def show
+        # binding.break
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was added successfully"
+            redirect_to article_path(@article)
+        else
+            flash[:error] = @article.errors.full_messages.join(' /n')
+            redirect_to new_article_path
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            flash[:error] = @article.errors.full_messages.join(' /n')
+            redirect_to edit_article_path 
+        end
+    end
+    
+    def destroy
+        if @article.destroy
+            redirect_to articles_path
+        else
+        end
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
